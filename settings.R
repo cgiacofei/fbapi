6c08dc0d44279c4f86dee15c81e0d4c706956334
chris <c.giacofei@gmail.com>
2015-10-28 13:27:35 +0100

    Move R files

--- settings.R
+++
@@ -1,5 +0,0 @@
-token <- 'eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE0NDY0NzU5MzgsInNjb3BlcyI6InJ3ZWkgcnBybyByaHIgcmxvYyBybnV0IHJzbGUgcnNldCByYWN0IHJzb2MiLCJzdWIiOiIzUzRSUFEiLCJhdWQiOiIyMjlSS1YiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJpYXQiOjE0NDU4ODc0MjV9.Vf6rjEey1Iaw-R99zXJQpF6KcjL1pF1S-eD9biPIL9Y'
-clientID <- "229RKV" # from dev.fitbit app manager
-
-fb.email <- 'c.giacofei@gmail.com'
-fb.password <-'Microe01'
