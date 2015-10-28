c6ae82ec0f1eb63280a8321674a95836f139d2c0
chris <c.giacofei@gmail.com>
2015-10-28 13:42:07 +0100

    Replace test code with functions

--- R/fitbit_api.R
+++ R/fitbit_api.R
@@ -1,35 +1,42 @@
-# a simple Oauth2.0 connection with Fitbit API
+# a simple Oauth connection with Fitbit API
 library(httr)
 
 source('settings.R')
 source('functions.R')
 source('dev.R')
 
-API_URL <- 'https://api.fitbit.com/1/user/-/'
+api_auth <- function(key, secret){
+  # Generate authentication token for fitbit API
+  #
+  # Inputs:
+  #  - key:   Fitbit API consumer key
+  #  - secret: Fitbit API consumer secret
+  #
+  # Returns: Token for interacting with API
 
-token_url = "https://api.fitbit.com/oauth/request_token"
-access_url = "https://api.fitbit.com/oauth/access_token"
-auth_url = "https://www.fitbit.com/oauth/authorize"
-
-key <- '31602c6029b39e65bd6ee6ed18499bf0'
-secret <- '724a62b928a0ed884bf5031f133009fd'
-
-fbr = oauth_app('data_access',key,secret)
-fitbit = oauth_endpoint(token_url,auth_url,access_url)
-token = oauth1.0_token(fitbit,fbr)
-sig = sign_oauth1.0(fbr, token=token$oauth_token, token_secret=token$oauth_token_secret)
-
-resp = GET(paste(API_URL,'activities/heart/date/2015-10-09/2015-10-26.json'), sig)
-content(resp)
-
-gs <- paste(API_URL,'activities/heart/date/2015-10-09/2015-10-26.json')
-heart.rate <- content(GET(gs,gtoken))
-
-gs <- paste(API_URL,'body/log/weight/2015-10-09/2015-10-26.json')
-weight <- content(GET(gs,gtoken))
-weight
-test <- NULL
-for (entry in weight$weight) {
-  test <- rbind(test, entry)
+  token_url = "https://api.fitbit.com/oauth/request_token"
+  access_url = "https://api.fitbit.com/oauth/access_token"
+  auth_url = "https://www.fitbit.com/oauth/authorize"
+    
+  fbr = oauth_app('data_access', key, secret)
+  fitbit = oauth_endpoint(token_url, auth_url, access_url)
+  token = oauth1.0_token(fitbit,fbr)
+  sig = sign_oauth1.0(fbr, token=token$oauth_token, token_secret=token$oauth_token_secret)
+  
+  return(sig)
 }
-rownames(test) <- NULL
+
+api_get <- function(url, token){
+  # Get data from fitbit API
+  #
+  # Inputs:
+  #  - url:   API url to grab data ie. 'activities/heart/date/2015-10-09/2015-10-26'
+  #  - token: Signature token returned from api_auth()
+  #
+  # Returns: Content of API reponse
+
+  API_URL <- 'https://api.fitbit.com/1/user/-/'
+  resp = GET(paste(API_URL, url,'.json'), token)
+  
+  return(content(resp))
+}
